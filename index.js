da614dd8747bf21bef58a5522d03029c3961ec84
Kevinava <kevinava1996@gmail.com>
2020-12-23 14:36:30 +0100

    Working

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = (client, message) => {
     const discord = require("discord.js");
     let text = false;
 
-    if (/(discord\.(gg|io|me|li)\/.+|discordapp\.com\/invite\/.+)/i.test(message.content)) return text = true;
-
+    if (/(discord\.(gg|io|me|li)\/.+|discordapp\.com\/invite\/.+)/i.test(message.content)) text = true;
+    
     if (text) {
         message.delete();
         message.channel.send(`Hey <@${message.author.id}>, you are not allowed to send spam. I sent you the message via DM to correct it.`);
